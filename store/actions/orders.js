f23ad73e6bd23756ec0f0d132963968b395b1f72
Michael <mgolden6@gmail.com>
2020-11-30 00:17:58 -0500

    store_orders

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,8 +1,37 @@
 export const ADD_ORDER = "ADD_ORDER";
 
 export const addOrder = (cartItems, totalAmount) => {
-  return {
-    type: ADD_ORDER,
-    orderData: { cartItems: cartItems, totalAmount: totalAmount },
+  return async (dispatch) => {
+    const date = new Date();
+    const response = await fetch(
+      `https://rn-shop-62a22.firebaseio.com/orders/u1.json`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          cartItems,
+          totalAmount,
+          date: date.toISOString(),
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Add Order didn't work!");
+    }
+
+    const resData = await response.json();
+
+    dispatch({
+      type: ADD_ORDER,
+      orderData: {
+        id: resData.name,
+        cartItems: cartItems,
+        totalAmount: totalAmount,
+        date: date,
+      },
+    });
   };
 };
