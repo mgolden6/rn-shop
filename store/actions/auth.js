d2812d733864277c42ad3ac18b9eb3f578f527ff
Michael <mgolden6@gmail.com>
2021-04-21 00:19:33 -0400

    run_expo_upgrade_to_v41

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,5 +1,5 @@
 import ENV from "../../env";
-import AsyncStorage from "@react-native-community/async-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const SIGNUP = "SIGNUP";
 export const SIGNIN = "SIGNIN";
